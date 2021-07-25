1284fe1cbb4bb5a70c60da88a5839260be9fe043
linhtran <ltran1@brynmawr.edu>
2021-07-26 00:11:21 +0700

    edit projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,26 +6,26 @@ app.controller('MainController', ['$scope', function($scope) {
         {
             title: 'Path-finding visualizer',
             text: "A visualizer tool for path-finding algorithms, created with JavaScript, Express.js, HTML/CSS. The tool allows user to create blocking wall between starting node and target node and choose an algorithm to visualize. The tool will animate the path-finding process. After the algorithm is done, the shortest path found will be highlighted. The whole project's credit goes to Clément Mihailescu - a great teacher whom I learn a tons from!",
-            image: 'assets/img/vv.png',
+            image: 'assets/img/pathfindingvisualizer.png',
             link: 'https://linhtran2407.github.io/PathFindingVisualizer/'
         },
         {
             title: 'Fake Caller',
-            text: 'An Android app that is created with Java, Android Studio, and Back4App for data storage. The app allows Android users to make fake call during dangerous or uncomfortable situation to avoid being followed. Users can also schedule fake incoming call, or make real call to 911 in case of real emergency when the app alone cannot help. The idea came from my personal experience when I was usually cat-called and followed by uneducated men at night. I really hope that the app, when published, can provide some help to other girls/women like me.',
-            image: 'assets/img/fakecaller.png',
+            text: 'An Android app that is created with Java, Android Studio, and Back4App for data storage. The app allows Android users to make fake call during dangerous or uncomfortable situation to avoid being followed. Users can also schedule fake incoming call, or make real call to 911 in case of real emergency when the app alone cannot help. The idea came from my personal experience when I was usually cat-called and followed by uneducated men at night. I and my team really hope that the app, when published, can provide some help to other girls/women like me.',
+            image: 'assets/img/b.png',
             link: 'https://github.com/app-project1/fake-caller-app'
         },
         {
             title: 'Together',
-            text: 'A web server developed using JavaScript, Node.js, Express.js, Mongoose, HTML/CSS. It allows users to ',
-            image: 'assets/img/this.png',
+            text: 'A web server developed using JavaScript, Node.js, Express.js, Mongoose, HTML/CSS. It allows users to create account, pair with other to make a couple, share and write diaries together, count together-days, and send real-time messages. This was my first project with Node.js and Express.js, which helped me learn really effectively the process of building a web server.',
+            image: 'assets/img/together.png',
             link: '#'
         },
         {
-            title: 'Upsilon Pi Epsilon',
-            text: 'I contributed to the web development committee at UPE, the Computer Science Honor Society at UC Berkeley. Me and a few others worked on adding new features to their website and improving the code base. Recently, we improved office hours such that officers could manage hours with a simple interface—no code knowledge needed. Also, we added the ability to schedule appointments for mock interviews and other things. It has been a great learning experience; I picked up some knowledge of the Django framework (and ultimately backend) here!',
-            image: 'assets/img/upe.png',
-            link: 'http://upe.berkeley.edu'
+            title: 'Personal website',
+            text: 'Yes, I do refer to this website as a project. This was built by JavaScript, Angular.js, and jQuery, which together provide me this single-page, simple, yet informative-enough website. It was a fun experience building this project.',
+            image: 'assets/img/this.png',
+            link: '#'
         },
         {
             title: 'Oppia',
